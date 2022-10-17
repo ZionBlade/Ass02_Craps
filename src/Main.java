9c904a5f6ce72ea11020f34879a03bfdb8cd6e52
ZionBlade <wernerct@mail.uc.edu>
2022-10-16 21:01:41 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -6,35 +7,84 @@ public class Main {
     {
         Scanner in = new Scanner(System.in);
         Random rnd = new Random();
-        boolean done = false;
+        boolean playing = false;
         String playAgain = "";
         String trash = "";
+        int newRoll = 0;
+        int die1 = rnd.nextInt(6)+1;
+        int die2 = rnd.nextInt(6)+1;
 
         do {
-            int die1 = rnd.nextInt(6)+1;
-            int die2 = rnd.nextInt(6)+1;
+            die1 = rnd.nextInt(6)+1;
+            die2 = rnd.nextInt(6)+1;
 
             int crapsRoll = die1 + die2;
 
             System.out.println("Die1 was: " + die1 + " and die2 was " + die2);
             System.out.println("The craps roll is " + crapsRoll);
+            System.out.println();
+            if(crapsRoll == 2 || crapsRoll == 3 || crapsRoll == 12)
+            {
+                System.out.println("Craps! You Lose!");
+            }
+            else if(crapsRoll == 7 || crapsRoll == 11)
+            {
+                System.out.println("That's a Natural! You Win!");
+            }
+            else
+            {
+                System.out.println("Keep playing until you get a 7 or a point sum");
+
+                while (newRoll != crapsRoll && newRoll != 7) {
+
+                    die1 = rnd.nextInt(6)+1;
+                    die2 = rnd.nextInt(6)+1;
+
+                    newRoll = die1 + die2;
+
+                    System.out.println("Die1 was: " + die1 + " and die2 was " + die2);
+                    System.out.println("The craps roll is " + newRoll);
+                    if(newRoll == crapsRoll)
+                    {
+                        System.out.println("That's Point! You Win!");
+
+                    }
+                    else if(newRoll == 7)
+                    {
+                        System.out.println("You Lose!");
+                    }
+                    else
+                    {
+                        System.out.println("Keep playing");
+                        System.out.println();
+                    }
+
+                }
+
+            }
 
             System.out.print("\nWould you like to play again? [Y/N]: ");
             if(in.hasNextLine())
             {
                 playAgain = in.nextLine();
-                in.nextLine();
-                if(playAgain.equalsIgnoreCase("Y"))
+                if(playAgain.equalsIgnoreCase("y"))
                 {
-                    done = true;
+                    playing = true;
                 }
-                if(playAgain.equalsIgnoreCase("N"))
+                else if(playAgain.equalsIgnoreCase("n"))
                 {
-                    
+                    playing = false;
+                }
+                else
+                {
+                    System.out.println("That is not a valid input, please input a valid input!");
                 }
             }
-        }while(!done);
-
-
+            else
+            {
+                System.out.println("That is not a valid input, please input a valid input!");
+            }
+        }while(playing);
     }
 }
+
